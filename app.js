bc6686aca6850ef7d399045a469c9d58ac2d37ec
karasmeitar <karasmeitar@gmail.com>
2016-12-20 22:32:19 +0200

    change

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(bodyParser.urlencoded({
 var allowCrossDomain = function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,contenttype');
+    res.setHeader('Access-Control-Allow-Headers', '*');
     res.setHeader('Access-Control-Allow-Credentials', true);
 
     if(req.method==='OPTIONS'){
